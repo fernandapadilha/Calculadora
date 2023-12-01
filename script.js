2584b1d42022677ebe41c0cacb1fe5e179849974
fernandapadilha <vitoriafernandaifc@gmail.com>
2023-12-01 16:51:53 -0300

    o tal dos gêmeos sui e cídio

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+/*é colocado dentro dos parenteses do querySelector oq queremos selecionar,
+neste caso sendo o .result(ponto usado por se referir a uma classe), a mesma coisa com o .igual*/
 const resultado = document.querySelector(".result");
 const confirmar = document.querySelector('.igual');
 
+/*O insert significa que eu quero inserir aluma coisa. */
 function insert(valor) {
     resultado.innerHTML += valor;
 }
 
+/*A função clean é usada para limpar tudo que tá dentro do <p> */
 function clean() {
     resultado.innerHTML = ' ';
 }
 
+/*backspace é para qunado eu quero apagar somente o ultimo digito */
 function backspace() {
     if(resultado.textContent) {
         let result = document.getElementById('resultado').innerHTML
@@ -16,6 +21,7 @@ function backspace() {
     }
 }
 
+/*confirma é para enviar o resultado final ao p quando clicar no = */
 function confirma() {
     if(resultado.textContent != 'Erro') {
         document.getElementById('resultado').innerHTML = eval(resultado.innerHTML)
